139a82f94ff6717d801093922e07a8a8e186662b
Andreas Persson <apersson508@gmail.com>
2026-03-30 13:15:49 +0200

    Backup innan optimering av seg Admin-panel

--- middleware.ts
+++ middleware.ts
@@ -105,8 +105,6 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    '/((?!_next/static|_next/image|blocked|favicon.ico).*)',
   ],
 }
-  ],
-}
+
