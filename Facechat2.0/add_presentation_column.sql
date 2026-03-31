3e3364633d13d329d4ebe363cc68a5aeb6912e57
Andreas Persson <apersson508@gmail.com>
2026-03-31 15:50:06 +0200

    Tog bort känsliga filer i tmp

--- Facechat2.0/add_presentation_column.sql
+++
@@ -1,2 +0,0 @@
--- Lägger till en kolumn för profilpresentation (Om Mig texten) i databasen
-ALTER TABLE public.profiles ADD COLUMN IF NOT EXISTS presentation TEXT DEFAULT '';
--- Facechat2.0/enable_snake_realtime.sql
+++
@@ -1,2 +0,0 @@
--- Aktivera Realtime för snake_scores så att vi kan visa tickers i Headern!
-alter publication supabase_realtime add table public.snake_scores;
